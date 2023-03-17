8b9727318f1063d849a2b2dcf1176f786301d63d
vikasgithub1122 <109681282+vikasgithub1122@users.noreply.github.com>
2023-03-17 21:56:28 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,80 +1,48 @@
-# # custom __format__() method
-# class Car:
-#     def __format__(self, format):
-#         if(format == 'colour'):
-#             return 'Red'
-#         return 'None'
-
-# print(format(Car(), "colour"))
-
-
-# class Human:
-#     def __init__(self,name,age,gender,):
-#         self.name=name
-#         self.age=age
-#         self.gender=gender
-# c=Human('raju',26,'male')
-# print(c.name)
-# print(c.gender)
-# print(c.age)
-
-
-# class Human:
-#     #class attribute
-#     species = "Homo Sapiens"
-#     def __init__(self, name, age, gender):
-#         self.name = name
-#         self.age = age
-#         self.gender = gender
+class Bank:
+    bank_name='SBI'
+    min_balance=500
+    
+    def __init__(self, accountNumber, name, balance):
+        self.accountNumber=accountNumber
+        self.name=name
+        self.balance=balance
         
-# x = Human("Ron", 15, "Male") 
-# y = Human("Miley", 22, "Female")
-# print(x.name)
-# print(y.name)
-# print(f'Hi! my name is {x.name} and i am a {x.gender} and i am {x.age}')
-# class car:
-#     def __init__(self, name):
-#         self.name=name
-#         def moves(self):
-#             print(f"{self.name} moves on the access")
-# c=car("honda")
-# print(c.name)
-
-
-class Human:
-    def __init__(self, name, age, gender):
-        self.name=name
-        self.age=age
-        self.gender=gender
+    def deposit (self, amount):
+        self.balance +=amount
+        print(f'deposit successfully')
+        
+    def details(self):
+        print(f"""account{self.accountnumber}")
+        name{self.name}
+        balance{self.balance}""")
+        
+    def withdrawl(self, amount):
+        self.balance -=amount
+        print(f'Withdraw successfully')
+        
+        # if self.balance<Bank.min_balance:
+        #     print("Please deposit amount with in a month, balance is very low")
+        # elif amount>self.balance:
+        #     print("your balance is", self.balance)
+        # elif amount<self.balance:
+        #     self.balance -=amount
     
-    def speak(self):
-        return f"Hello everyone! i am {self.name}"
-    
-    def eat(self, favouriteDish):
-        return f"I love to eat {favouriteDish}!!!"
-    
-    def car(self, car_name):
-        return f"i love {car_name}"
+b=Bank(1001, 'virat', 6500)
+print("W: Withdrawl")
+print("D: deposit")
+print("A:Details")
+print("q:Exit")
 
-x=Human("Ciri", 18, "female")
-print(x.speak())
-print(x.eat("momos"))
-print(x.car("black scorpio"))        
-
-
-
-class Human:
-    def __init__(self, name, age, gender):
-        self.name=name
-        self.age=age
-        self.gender=gender
-    
-    def discription(self):
-        print(f"Hey! My name is {self.name}, i'm a {self.gender} and I'm {self.age}years old")
-class Boy(Human):
-    def schoolName(self,schoolName):
-        print("i study in {schoolName}")
-h=Human('lily', 32, 'girl')
-h.discription()
-h.schoolName('ABC Academy')
+while True:
+    key=input("Enter a Key :")
+    if key=="W":
+        amount=float(input("Enter a amount"))
+        b.withdrawl(amount)
+    elif key=="D":
+        amount=float(input("Enter a amount:"))
+        b.deposit(amount)
+    elif key=="A":
+        b.details()
+    elif key=='q':
+        break
     
